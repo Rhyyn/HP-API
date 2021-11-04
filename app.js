0872be7fdcb7cd25ffea25f7ea559287dc7d3600
Rhyn <tonymigeonpro@gmail.com>
2021-11-04 11:46:18 +0100

    added host as an alternate localhost to fix HEROKU bind to PORT

--- app.js
+++ app.js
@@ -62,8 +62,9 @@ app.delete('/characters/:id', async (req, res) =>{
     }
 })
 
+const host = '0.0.0.0';
 const port = process.env.port || 3000;
 
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+app.listen(port, host, () => {
+    console.log(`Server succesfully started on port ${port}`);
 })
